e291b340ff626a8a8560eede75903d2e776d6f79
Jakub Myrta <mkuba2006@gmail.com>
2023-08-20 13:33:44 +0200

    9

--- src/components/topic/main.js
+++ src/components/topic/main.js
@@ -2,6 +2,7 @@ import React from "react";
 import classes from './style.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 
 function Topic() {
@@ -10,6 +11,9 @@ function Topic() {
             <div className={classes.first}>
                 <h1>Enhance style with premium products</h1>
                 <h4>Upgrade your daily routine with our premium men's hair products. Experience confidence through exceptional grooming.</h4>
+                <a href="#">
+                    <button>Explore <FontAwesomeIcon icon={faArrowRight} /></button>
+                </a>
             </div>   
             <div className={classes.imageContainer}>
                 <img
@@ -19,9 +23,9 @@ function Topic() {
             </div>
              <div className={classes.third}>
                 <h5 className={classes.follow}>FOLLOW US</h5>
-                <FontAwesomeIcon icon={faInstagram} />
-                <FontAwesomeIcon icon={faFacebook} />
-                <FontAwesomeIcon icon={faGithub} />
+                <FontAwesomeIcon icon={faInstagram} className={classes.icon}/>
+                <FontAwesomeIcon icon={faFacebook} className={classes.icon}/>
+                <FontAwesomeIcon icon={faGithub} className={classes.icon}/>
             </div>
         </main>
     );
