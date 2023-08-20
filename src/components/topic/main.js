1dd189e729094c2ed0e58ead43686de4a4d34972
Jakub Myrta <mkuba2006@gmail.com>
2023-08-20 12:21:22 +0200

    2

--- src/components/topic/main.js
+++ src/components/topic/main.js
@@ -11,7 +11,7 @@ function Topic() {
                 <h1>We have the best hair products for you</h1>
                 <h4>Upgrade your daily routine with our premium men's hair products. Experience confidence through exceptional grooming.</h4>
             </div>   
-             <img src="" alt="Meraki"/>
+             <img src="https://github.com/mkuba2006/site/blob/master/public/images/Blumaan.jpg?raw=true" alt="Meraki"/>
              <div className={classes.third}>
                 <h5 className={classes.follow}>FOLLOW US</h5>
                 <FontAwesomeIcon icon={faInstagram} />
